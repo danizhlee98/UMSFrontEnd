1d4dacbbfd1bbb66f79a828a1056c810147e92f6
Danizhlee98 <danizhlee98@gmail.com>
2025-08-22 00:19:17 +0800

    feat(auth): add GetAccessToken method in AuthRouter

--- src/app/router/authRouter.ts
+++ src/app/router/authRouter.ts
@@ -1,21 +1,30 @@
 import api from "../axios/api";
-import { LoginRequest, LoginResponse, UserResponse } from "../schema/userSchema";
+import {
+  LoginRequest,
+  LoginResponse,
+  UserResponse,
+} from "../schema/userSchema";
 
+let accessToken: string | null = null;
 
 export const authRouter = {
-    login: async (data: LoginRequest): Promise<UserResponse> => {
-      try{
-        const response = await api.post("/Auth/login", data);
-        console.log("response login:",response);
-        return response.data;
-      }
-      catch (error){
-        console.log("Error Login:",error);
-        throw error;
-      }
-    },
-    logout: async (): Promise<void> => {
-      await api.post("/logout");
-      localStorage.removeItem("token");
-    },
-  };
+  login: async (data: LoginRequest): Promise<UserResponse> => {
+    try {
+      const response = await api.post("/Auth/login", data);
+      accessToken = response.data.accessToken;
+      console.log("response login:", response);
+      return response.data;
+    } catch (error) {
+      console.log("Error Login:", error);
+      throw error;
+    }
+  },
+  getAccessToken: () => accessToken,
+  clearAccessToken: () => {
+    accessToken = null;
+  },
+  logout: async (): Promise<void> => {
+    await api.post("/logout");
+    localStorage.removeItem("token");
+  },
+};
