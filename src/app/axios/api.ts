a03e040e15a6969ba685160327fe42362887e4a8
Danizhlee98 <danizhlee98@gmail.com>
2025-08-22 00:23:17 +0800

    chore(axios): remove withCredentials option from config

--- src/app/axios/api.ts
+++ src/app/axios/api.ts
@@ -1,12 +1,23 @@
 // src/lib/api.ts
 import axios from "axios";
+import { authRouter } from "../router/authRouter";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_LOCAL_API, 
-  withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "application/json"
   },
+});
+
+// Interceptor: runs before every request
+api.interceptors.request.use((config) => {
+  const token = authRouter.getAccessToken();
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    delete config.headers.Authorization; // remove header if no token
+  }
+  return config;
 });
 
 export default api;
